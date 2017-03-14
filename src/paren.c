3742308c13f63d853f855252cd78916207749206
mukaiizawa <mukaiizawa@yahoo.co.jp>
2017-03-14 12:58:46 +0900

    Update

--- src/paren.c
+++ src/paren.c
@@ -1,3 +1,19 @@
 /*
   paren main routine.
 */
+
+#include <stdio.h>
+
+static char prompt[2048];
+
+char* read() {
+  return fgets(prompt, 2048, stdin);
+}
+
+int main(int argc, char* argv[]) {
+  while (1) {
+    fputs(") ", stdout);
+    printf("%s", read());
+  }
+  return 0;
+}
