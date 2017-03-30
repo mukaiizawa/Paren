311200217590b1225167c319d272bfd9ec024240
mukaiizawa <mukaiizawa@yahoo.co.jp>
2017-03-31 08:59:33 +0900

    Update queue

--- src/queue.c
+++ src/queue.c
@@ -2,62 +2,59 @@
 #include <stdlib.h>
 
 typedef struct {
-    struct node {
-        int value;
-        struct node *next;
-    } *top, *tail;
+  struct Queue_node {
+    int value;
+    struct Queue_node *next;
+  } *top, *tail;
 } Queue;
 
 void Queue_put(Queue*, int);
 int Queue_get(Queue*);
 void Queue_print(Queue*);
 
-void Queue_put(Queue* q, int n){
-  struct node *new_node = malloc(sizeof(*new_node));
+void Queue_put(Queue* q, int n) {
+  struct Queue_node *new_node = malloc(sizeof(*new_node));
   new_node->value = n;
   new_node->next = NULL;
-  if(q->top == NULL)
+  if (q->top == NULL)
     q->top = q->tail = new_node;
   else 
     q->tail = q->tail->next = new_node;
 }
 
-int Queue_get(Queue* q){
-    if(q->top == NULL){
-        fprintf(stderr, "queque is empty. can't get\n");
-        exit(-1);
-    }
-    struct node *temp = q->top;
-    q->top = temp->next;
-    int ret_value = temp->value;
-    free(temp);
-    return ret_value;
+int Queue_get(Queue* q) {
+  if (q->top == NULL) {
+    fprintf(stderr, "queque is empty. can't get\n");
+    exit(-1);
+  }
+  struct Queue_node *temp = q->top;
+  q->top = temp->next;
+  int ret_value = temp->value;
+  free(temp);
+  return ret_value;
 }
 
-void Queue_print(Queue* q){
-    struct node *temp = q->top;
-    printf("queue:");
-    while(temp){
-        printf(" %d", temp->value);
-        temp = temp->next;
-    }
-    puts("");
+void Queue_print(Queue* q) {
+  struct Queue_node *node;
+  printf("queue: [");
+  for(node = q->top; node; node = node->next)
+    printf(" %d", node->value);
+  puts(" ]");
 }
 
 int main(void){
-    int i,n;
-    Queue q = { NULL, NULL};
-    srand(1);
-    for (i = 0; i < 3; i++) {
-        n = rand() % 100;
-        printf("enqueue %d\n", n);
-        Queue_put(&q, n);
-        Queue_print(&q);
-    }
-    for(i = 0; i < 3; i++) {
-        printf("dequeue %d\n", Queue_get(&q));
-        Queue_print(&q);
-    }
-    return 0;
+  int i,n;
+  Queue q = { NULL, NULL};
+  srand(1);
+  for (i = 0; i < 3; i++) {
+    n = rand() % 100;
+    printf("enqueue %d\n", n);
+    Queue_put(&q, n);
+  }
+    Queue_print(&q);
+  for(i = 0; i < 3; i++) {
+    printf("dequeue %d\n", Queue_get(&q));
+    Queue_print(&q);
+  }
+  return 0;
 }
-
