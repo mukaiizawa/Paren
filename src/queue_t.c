e230fa4154a0671f281bac304283bcf7d734540f
mukaiizawa <mukaiizawa@yahoo.co.jp>
2017-04-03 08:35:19 +0900

    Update test

--- src/queue_t.c
+++ src/queue_t.c
@@ -1,19 +1,15 @@
+#include <string.h>
+#include <assert.h>
 #include "queue.h"
 
 int main(void) {
-  int i,n;
   Queue q;
   Queue_init(&q);
-  Queue_enqueue(&q, "1");
-  Queue_enqueue(&q, "2");
-  Queue_enqueue(&q, "3");
-  Queue_dump(&q);
-  // get
-  Queue_dequeue(&q);
-  Queue_dump(&q);
-  Queue_dequeue(&q);
-  Queue_dump(&q);
-  Queue_dequeue(&q);
-  Queue_dump(&q);
+  Queue_enqueue(&q, "first");
+  Queue_enqueue(&q, "second");
+  Queue_enqueue(&q, "third");
+  assert(strcmp(Queue_dequeue(&q), "first") == 0);
+  assert(strcmp(Queue_dequeue(&q), "second") == 0);
+  assert(strcmp(Queue_dequeue(&q), "third") == 0);
   return 0;
 }
