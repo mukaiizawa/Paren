9a978cebd58142e19a6463fe9df21258cd6c3491
mukaiizawa <mukaiizawa@yahoo.co.jp>
2017-04-12 16:57:26 +0900

    Add env.c

--- src/env.c
+++ src/env.c
@@ -7,7 +7,7 @@
 #include <string.h>
 
 struct Env {
-  struct Env *prev, *next;
+  struct Env *top, *bottom;
   struct EnvNode {
     char *key;
     char *type;
@@ -17,20 +17,38 @@ struct Env {
 };
 
 int Env_isRootEnv(struct Env *env) {
-  return env->prev == env;
+  return env->top == NULL;
+}
+
+int Env_isBotommostEnv(struct Env *env) {
+  return env->bottom == env;
 }
 
 int Env_isLastEnvNode(struct Env *env, struct EnvNode *envNode) {
-  return env == envNode->next;
+  return env->node == envNode->next;
+}
+
+struct Env *Env_bottommost(struct Env *env) {
+  while (Env_isBotommostEnv(env))
+    env = env->bottom;
+  return env;
 }
 
 struct EnvNode *Env_lookup(struct Env *env, char *key) {
-  EnvNode node;
+  struct EnvNode *node;
+  env = Env_bottommost(env);
   while (!Env_isRootEnv(env)) {
     node = env->node;
-    while (!Env_isLastEnvNode(node)) {
-      if (strcmp(node->key, 
+    while (!Env_isLastEnvNode(env, node)) {
+      if (strcmp(node->key, key) == 0) {
+        return node;
+      }
     }
+    env = env->top;
   }
   return NULL;
+}
+
+int main(void) {
+  return 0;
 }
