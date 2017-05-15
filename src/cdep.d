667745ab38cc6a059b535eb150a7ad3da555796b
mukaiizawa <mukaiizawa@yahoo.co.jp>
2017-05-15 10:14:13 +0900

    Update c dependency

--- src/cdep.d
+++ src/cdep.d
@@ -1,8 +1,8 @@
-ahdrd.o: ahdrd.c ahdrd.h ringbuf.h
-env.o: env.c env.h prim.h sexpr.h
-lex.o: lex.c lex.h ahdrd.h ringbuf.h prim.h sexpr.h
-paren.o: paren.c env.h lex.h prim.h sexpr.h
-prim.o: prim.c prim.h sexpr.h
+ahdrd.o: ahdrd.c ringbuf.h ahdrd.h
+env.o: env.c env.h splay.h prim.h
+lex.o: lex.c ringbuf.h ahdrd.h lex.h splay.h prim.h
+paren.o: paren.c env.h ringbuf.h ahdrd.h lex.h splay.h prim.h
+prim.o: prim.c splay.h prim.h
 ringbuf.o: ringbuf.c ringbuf.h
-sexpr.o: sexpr.c sexpr.h
+splay.o: splay.c splay.h
 
