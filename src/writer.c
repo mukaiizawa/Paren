49096067d58ec7a8efcf2153e7e2b303677f14c0
mukaiizawa <mukaiizawa@yahoo.co.jp>
2017-06-06 10:04:14 +0900

    Fix type case Function

--- src/writer.c
+++ src/writer.c
@@ -29,7 +29,8 @@ void Writer_write(Writer *wr, S *expr) {
     if (fraction == 0) fprintf(fp, "%d", (int)intptr); 
     else fprintf(fp, "%f", expr->Number.val);
   }
-  else if (TYPEP(expr, Function)) fprintf(fp, "%p", expr);
+  // TODO: writer for function.
+  // else if (TYPEP(expr, Function)) fprintf(fp, "%p", expr);
   else if (TYPEP(expr, Error)) fprintf(fp, "%s", expr->Error.val);
   else if (TYPEP(expr, Cons)) {
     fprintf(fp, "(");
