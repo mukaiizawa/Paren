fcbcd7caf078c1112f58c950ac3e0ac36e4c0c3f
mukaiizawa <mukaiizawa@yahoo.co.jp>
2017-06-14 07:36:51 +0900

    Create xstrcat

--- src/std.c
+++ src/std.c
@@ -10,26 +10,36 @@
 
 #include "std.h"
 
+#define xerror(str) { \
+  fprintf(stderr, str); \
+  exit(1); \
+}
+
 void *xmalloc(int size) {
   void *p;
   assert(size > 0);
-  if((p = malloc(size)) == NULL) {
-    fprintf(stderr, "xmalloc: Cannot allocate memory.");
-    exit(1);
-  }
+  if ((p = malloc(size)) == NULL) xerror("xmalloc: Cannot allocate memory.");
   return p;
 }
 
-char *xmkstr(int size, char *fmt, ...) {
+void *xrealloc(void *p, int size) {
+  if (p == NULL) return xmalloc(size);
+  else if ((p = realloc(p, size)) == NULL)
+    xerror("xmalloc: Cannot allocate memory.");
+  return p;
+}
+
+char *_xvstrcat(char *s1, ...) {
   int len;
-  char *buf;
-  va_list va;
-  buf = xmalloc(size);
-  va_start(va,fmt);
-  if ((len = vsprintf(buf, fmt, va)) < 0 || len >= size) {
-    fprintf(stderr, "xmkstr: Buffer over flow.");
-    exit(1);
+  char *s, *buf;
+  va_list args;
+  va_start(args, s1);
+  buf = xmalloc(len = sizeof(char));
+  buf[0] = '\0';
+  for (s = s1; s != NULL; s = va_arg(args, char *)) {
+    xrealloc(buf, len += (strlen(s) + 1));
+    buf = strcat(buf, s);
   }
-  va_end(va);
+  va_end(args);
   return buf;
 }
--- src/std.h
+++ src/std.h
@@ -2,7 +2,7 @@
   paren standard library.
 */
 
-#define MAX_STR_LEN 256
-
 extern void *xmalloc(int size);
-char *xmkstr(int size, char *fmt, ...);
+extern void *xrealloc(void *p, int size);
+#define xvstrcat(...) _xvstrcat(__VA_ARGS__, NULL)
+extern char *_xvstrcat(char *s1, ...);    // use xvstrcat!
