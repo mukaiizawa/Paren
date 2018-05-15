a3f15fb168298b7dca3f05b9a1f89130a462076d
mukaiizawa <mukaiizawa@yahoo.co.jp>
2018-05-16 07:49:31 +0900

    Minor changes

--- src/bi.c
+++ src/bi.c
@@ -9,19 +9,16 @@
 #undef SPECIAL
 #undef PRIM
 
-char *special_name_table[] = {
-#define SPECIAL(name, sym) #sym,
-#include "special.wk"
-#undef SPECIAL
-  NULL
-};
-
 #define SPECIAL(name, sym) extern object special_##name(object, int, object);
 #include "special.wk"
 #undef SPECIAL
 
-object (*special_table[])(object env, int argc, object argv) = {
-#define SPECIAL(name, sym) special_##name,
+#define PRIM(name) extern int prim_##name(int, object, object *);
+#include "prim.wk"
+#undef PRIM
+
+char *special_name_table[] = {
+#define SPECIAL(name, sym) #sym,
 #include "special.wk"
 #undef SPECIAL
   NULL
@@ -34,9 +31,12 @@ char *prim_name_table[] = {
   NULL
 };
 
-#define PRIM(name) extern int prim_##name(int, object, object *);
-#include "prim.wk"
-#undef PRIM
+object (*special_table[])(object env, int argc, object argv) = {
+#define SPECIAL(name, sym) special_##name,
+#include "special.wk"
+#undef SPECIAL
+  NULL
+};
 
 int (*prim_table[])(int argc, object argv, object *result) = {
 #define PRIM(name) prim_##name,
