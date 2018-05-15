677ffe4768821394803ff843a248ce80287caf06
mukaiizawa <mukaiizawa@yahoo.co.jp>
2018-05-16 07:14:31 +0900

    Refactored

--- src/prim.c
+++ src/prim.c
@@ -8,6 +8,6 @@
 
 object prim_xint(int64_t val)
 {
-  if(XINT_MIN <= val && val <= XINT_MAX) return gc_new_xint(val);
+  if (XINT_MIN <= val && val <= XINT_MAX) return gc_new_xint(val);
   return gc_new_xfloat(val);
 }
