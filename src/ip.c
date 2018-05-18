522f780bded533fb9ea977a02e28d6b30ae8be08
mukaiizawa <mukaiizawa@yahoo.co.jp>
2018-05-18 13:22:05 +0900

    Improved

--- src/ip.c
+++ src/ip.c
@@ -169,20 +169,18 @@ static object eval_sequential(object env, object expr)
 static object apply(object operator, object operands)
 {
   int rest_p;
-  char *s;
-  object e, k, v, params, result;
-
+  object e, k, v, params;
   rest_p = FALSE;
   e = gc_new_env(operator->lambda.env);
   params = operator->lambda.params;
-
+  // required parameter
   while (params != object_nil && !typep(params->cons.car, Keyword)) {
     if (operands == object_nil) xerror("too few arguments");
     xsplay_add(&e->env.binding, params->cons.car, operands->cons.car);
     params = params->cons.cdr;
     operands = operands->cons.cdr;
   }
-
+  // optional parameter
   if (params->cons.car == object_opt) {
     params = params->cons.cdr;
     while (params != object_nil && !typep(params->cons.car, Keyword)) {
@@ -201,14 +199,14 @@ static object apply(object operator, object operands)
       xsplay_add(&e->env.binding, k, v);
     }
   }
-
+  // rest parameter
   if (params->cons.car == object_rest) {
     params = params->cons.cdr;
     xsplay_add(&e->env.binding, params->cons.car, operands);
     params = params->cons.cdr;
     rest_p = TRUE;
   }
-
+  // keyword parameter
   if (params->cons.car == object_key) {
     params = params->cons.cdr;
     while (params != object_nil) {
@@ -225,8 +223,7 @@ static object apply(object operator, object operands)
     while (operands != object_nil) {
       if (!typep(operands->cons.car, Keyword))
         xerror("illegal keyword parameter");
-      s = operands->cons.car->symbol.name + 1;
-      k = gc_new_symbol(s);    // skip ':'
+      k = gc_new_symbol(operands->cons.car->symbol.name + 1);    // skip ':'
       if ((v = xsplay_find(&e->env.binding, k)) == NULL)
         xerror("undefined keyword parameter ':%s'", k->symbol.name);
       v = (operands = operands->cons.cdr)->cons.car;
@@ -234,11 +231,8 @@ static object apply(object operator, object operands)
       operands = operands->cons.cdr;
     }
   }
-
   if (!rest_p && operands != object_nil) xerror("too many arguments");
-
-  result = eval_sequential(e, operator->lambda.body);
-  return result;
+  return eval_sequential(e, operator->lambda.body);
 }
 
 static object eval(object env, object expr)
